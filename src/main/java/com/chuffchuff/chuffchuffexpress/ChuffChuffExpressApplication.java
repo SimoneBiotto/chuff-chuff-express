f9dd255415d868f58a31d382325558adc26ea276
zuppa <smn01b@gmail.com>
2024-02-25 16:11:47 +0100

    first commit

---
+++ src/main/java/com/chuffchuff/chuffchuffexpress/ChuffChuffExpressApplication.java
@@ -0,0 +1,13 @@
+package com.chuffchuff.chuffchuffexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuffChuffExpressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuffChuffExpressApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chuffchuff/chuffchuffexpress/ChuffChuffExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuffchuff.chuffchuffexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuffChuffExpressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
