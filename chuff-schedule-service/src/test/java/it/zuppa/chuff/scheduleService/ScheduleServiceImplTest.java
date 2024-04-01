5e1ea367ad9b001d7a9124bdfc6724dc1cb67d51
Zuppa <smn01b@gmail.com>
2024-04-01 17:59:45 +0200

    add schedule service module test

---
+++ chuff-schedule-service/src/test/java/it/zuppa/chuff/scheduleService/ScheduleServiceImplTest.java
@@ -0,0 +1,195 @@
+package it.zuppa.chuff.scheduleService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import it.zuppa.chuff.domain.schedule.Schedule;
+import it.zuppa.chuff.domain.schedule.ScheduleType;
+import it.zuppa.chuff.exception.DomainException;
+import it.zuppa.chuff.scheduleService.dto.schedule.CreateScheduleRequest;
+import it.zuppa.chuff.scheduleService.dto.schedule.ScheduleResponse;
+import it.zuppa.chuff.scheduleService.mapper.ScheduleDataMapper;
+import it.zuppa.chuff.scheduleService.ports.output.repositoty.ScheduleRepository;
+import java.security.InvalidParameterException;
+import java.time.DayOfWeek;
+import java.util.Optional;
+import java.util.UUID;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ScheduleServiceImplTest {
+  @Mock private ScheduleRepository repository;
+  @Mock private ScheduleDataMapper mapper;
+  private ScheduleServiceImpl service;
+
+  @BeforeAll
+  public void setup() {
+    repository = Mockito.mock(ScheduleRepository.class);
+    mapper = Mockito.mock(ScheduleDataMapper.class);
+    service = new ScheduleServiceImpl(repository, mapper);
+  }
+
+  @AfterEach
+  public void reset() {
+    Mockito.reset(repository, mapper);
+  }
+
+  @Test
+  public void itShouldCreateScheduleWhenCreateScheduleRequestIsCorrect() throws DomainException {
+    CreateScheduleRequest createScheduleRequest =
+        CreateScheduleRequest.builder()
+            .type(ScheduleType.DAILY)
+            .priceIncreasedPercentageOnFestive(1)
+            .weekDay(DayOfWeek.of(1))
+            .build();
+    Schedule schedule =
+        Schedule.builder()
+            .type(createScheduleRequest.type())
+            .priceIncreasedPercentageOnFestive(
+                createScheduleRequest.priceIncreasedPercentageOnFestive())
+            .weekDay(createScheduleRequest.weekDay())
+            .build();
+    schedule.setId(UUID.randomUUID());
+    ScheduleResponse scheduleResponse =
+        ScheduleResponse.builder()
+            .id(schedule.getId())
+            .type(schedule.getType())
+            .priceIncreasedPercentageOnFestive(schedule.getPriceIncreasedPercentageOnFestive())
+            .weekDay(schedule.getWeekDay())
+            .enabled(schedule.isEnabled())
+            .build();
+    Mockito.when(repository.createSchedule(schedule)).thenReturn(schedule);
+    Mockito.when(mapper.createScheduleRequestoToSchedule(createScheduleRequest))
+        .thenReturn(schedule);
+    Mockito.when(mapper.scheduleToScheduleResponse(schedule)).thenReturn(scheduleResponse);
+    ScheduleResponse response = service.createSchedule(createScheduleRequest);
+    Mockito.verify(mapper, Mockito.times(1))
+        .createScheduleRequestoToSchedule(createScheduleRequest);
+    assertEquals(scheduleResponse.id(), response.id());
+    assertEquals(scheduleResponse.type(), response.type());
+    assertEquals(
+        scheduleResponse.priceIncreasedPercentageOnFestive(),
+        response.priceIncreasedPercentageOnFestive());
+    assertEquals(scheduleResponse.weekDay(), response.weekDay());
+    assertEquals(scheduleResponse.enabled(), response.enabled());
+  }
+
+  @Test
+  public void itShouldThrowExceptionWhenScheduleIsNotSaved() {
+    CreateScheduleRequest createScheduleRequest =
+        CreateScheduleRequest.builder()
+            .type(ScheduleType.DAILY)
+            .priceIncreasedPercentageOnFestive(1)
+            .weekDay(DayOfWeek.of(1))
+            .build();
+    Schedule schedule =
+        Schedule.builder()
+            .type(createScheduleRequest.type())
+            .priceIncreasedPercentageOnFestive(
+                createScheduleRequest.priceIncreasedPercentageOnFestive())
+            .weekDay(createScheduleRequest.weekDay())
+            .build();
+    Mockito.when(repository.createSchedule(schedule)).thenReturn(null);
+    Mockito.when(mapper.createScheduleRequestoToSchedule(createScheduleRequest))
+        .thenReturn(schedule);
+    DomainException exception =
+        assertThrows(DomainException.class, () -> service.createSchedule(createScheduleRequest));
+    assertEquals(DomainException.Reason.ERROR_DURING_SAVING, exception.getReason());
+    assertEquals(Schedule.class, exception.getDomainClass());
+  }
+
+  @Test
+  public void itShouldDeleteScheduleWhenIdIsCorrect() throws DomainException {
+    UUID id = UUID.randomUUID();
+    Schedule schedule = Schedule.builder().type(ScheduleType.DAILY).build();
+    schedule.setId(id);
+    Mockito.when(repository.findById(id)).thenReturn(Optional.of(schedule));
+    service.deleteSchedule(id);
+    Mockito.verify(repository, Mockito.times(1)).deleteSchedule(schedule);
+  }
+
+  @Test
+  public void itShouldThrowExceptionWhenIdIsNull() {
+    UUID id = null;
+    InvalidParameterException exception =
+        assertThrows(InvalidParameterException.class, () -> service.deleteSchedule(id));
+  }
+
+  @Test
+  public void itShouldThrowExceptionWhenScheduleIsNotFound() {
+    UUID id = UUID.randomUUID();
+    Mockito.when(repository.findById(id)).thenReturn(Optional.empty());
+    DomainException exception =
+        assertThrows(DomainException.class, () -> service.deleteSchedule(id));
+    assertEquals(DomainException.Reason.RESOURCE_NOT_FOUND, exception.getReason());
+    assertEquals(Schedule.class, exception.getDomainClass());
+  }
+
+  @Test
+  public void itShouldToggleEnabledStatusOfScheduleWhenIdIsCorrect() throws DomainException {
+    UUID id = UUID.randomUUID();
+    Schedule schedule = Schedule.builder().type(ScheduleType.DAILY).build();
+    schedule.setId(id);
+    Mockito.when(repository.findById(id)).thenReturn(Optional.of(schedule));
+    Mockito.when(repository.updateSchedule(schedule)).thenReturn(Optional.of(schedule));
+    ScheduleResponse scheduleResponse =
+        ScheduleResponse.builder()
+            .id(schedule.getId())
+            .type(schedule.getType())
+            .priceIncreasedPercentageOnFestive(schedule.getPriceIncreasedPercentageOnFestive())
+            .weekDay(schedule.getWeekDay())
+            .enabled(schedule.isEnabled())
+            .build();
+    Mockito.when(mapper.scheduleToScheduleResponse(schedule)).thenReturn(scheduleResponse);
+    ScheduleResponse response = service.toggleEnabledStatusSchedule(id, !schedule.isEnabled());
+    Mockito.verify(repository, Mockito.times(1)).updateSchedule(schedule);
+    assertEquals(scheduleResponse.id(), response.id());
+    assertEquals(scheduleResponse.type(), response.type());
+    assertEquals(
+        scheduleResponse.priceIncreasedPercentageOnFestive(),
+        response.priceIncreasedPercentageOnFestive());
+    assertEquals(scheduleResponse.weekDay(), response.weekDay());
+    assertEquals(scheduleResponse.enabled(), response.enabled());
+  }
+
+  @Test
+  public void itShouldThrowExceptionWhenScheduleIsNotFoundForToggleEnabledStatus() {
+    UUID id = UUID.randomUUID();
+    Mockito.when(repository.findById(id)).thenReturn(Optional.empty());
+    DomainException exception =
+        assertThrows(DomainException.class, () -> service.toggleEnabledStatusSchedule(id, true));
+    assertEquals(DomainException.Reason.RESOURCE_NOT_FOUND, exception.getReason());
+    assertEquals(Schedule.class, exception.getDomainClass());
+  }
+
+  @Test
+  public void itShouldReturnScheduleWithDesiredStatusWhenScheduleAlreadyHasDesiredStatus()
+      throws DomainException {
+    UUID id = UUID.randomUUID();
+    Schedule schedule = Schedule.builder().type(ScheduleType.DAILY).build();
+    schedule.setId(id);
+    Mockito.when(repository.findById(id)).thenReturn(Optional.of(schedule));
+    ScheduleResponse scheduleResponse =
+        ScheduleResponse.builder()
+            .id(schedule.getId())
+            .type(schedule.getType())
+            .priceIncreasedPercentageOnFestive(schedule.getPriceIncreasedPercentageOnFestive())
+            .weekDay(schedule.getWeekDay())
+            .enabled(schedule.isEnabled())
+            .build();
+    Mockito.when(mapper.scheduleToScheduleResponse(schedule)).thenReturn(scheduleResponse);
+    ScheduleResponse response = service.toggleEnabledStatusSchedule(id, schedule.isEnabled());
+    assertEquals(scheduleResponse.id(), response.id());
+    assertEquals(scheduleResponse.type(), response.type());
+    assertEquals(
+        scheduleResponse.priceIncreasedPercentageOnFestive(),
+        response.priceIncreasedPercentageOnFestive());
+    assertEquals(scheduleResponse.weekDay(), response.weekDay());
+    assertEquals(scheduleResponse.enabled(), response.enabled());
+  }
+}
---
+++ chuff-schedule-service/src/test/java/it/zuppa/chuff/scheduleService/mapper/ScheduleDataMapperTest.java
@@ -0,0 +1,87 @@
+package it.zuppa.chuff.scheduleService.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import it.zuppa.chuff.domain.schedule.Schedule;
+import it.zuppa.chuff.domain.schedule.ScheduleType;
+import it.zuppa.chuff.scheduleService.dto.schedule.CreateScheduleRequest;
+import it.zuppa.chuff.scheduleService.dto.schedule.ScheduleResponse;
+import java.time.DayOfWeek;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class ScheduleDataMapperTest {
+  private final ScheduleDataMapper mapper = new ScheduleDataMapper();
+
+  @ParameterizedTest
+  @MethodSource("provideCreateScheduleRequestForItShouldReturnScheduleFromCreateScheduleRequest")
+  public void itShouldReturnScheduleFromCreateScheduleRequest(CreateScheduleRequest request) {
+    Schedule schedule = mapper.createScheduleRequestoToSchedule(request);
+    assertTrue(schedule.isEnabled());
+    assertEquals(request.type(), schedule.getType());
+    assertEquals(
+        request.priceIncreasedPercentageOnFestive(),
+        schedule.getPriceIncreasedPercentageOnFestive());
+    assertEquals(request.weekDay(), schedule.getWeekDay());
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideScheduleForItShouldReturnScheduleResponseFromSchedule")
+  public void itShouldReturnScheduleResponseFromSchedule(Schedule schedule) {
+    ScheduleResponse response = mapper.scheduleToScheduleResponse(schedule);
+    assertEquals(schedule.getType(), response.type());
+    assertEquals(
+        schedule.getPriceIncreasedPercentageOnFestive(),
+        response.priceIncreasedPercentageOnFestive());
+    assertEquals(schedule.getWeekDay(), response.weekDay());
+    assertEquals(schedule.getId(), response.id());
+    assertEquals(schedule.isEnabled(), response.enabled());
+  }
+
+  private static Stream<Arguments>
+      provideCreateScheduleRequestForItShouldReturnScheduleFromCreateScheduleRequest() {
+    return Stream.of(
+        Arguments.of(CreateScheduleRequest.builder().type(ScheduleType.OCCASIONALLY).build()),
+        Arguments.of(
+            CreateScheduleRequest.builder()
+                .type(ScheduleType.WEEKLY)
+                .weekDay(DayOfWeek.of(1))
+                .build()),
+        Arguments.of(
+            CreateScheduleRequest.builder()
+                .type(ScheduleType.DAILY)
+                .priceIncreasedPercentageOnFestive(10)
+                .build()),
+        Arguments.of(
+            CreateScheduleRequest.builder()
+                .type(ScheduleType.WORKDAY)
+                .priceIncreasedPercentageOnFestive(2)
+                .build()),
+        Arguments.of(CreateScheduleRequest.builder().type(ScheduleType.WEEKEND).build()));
+  }
+
+  private static Stream<Arguments> provideScheduleForItShouldReturnScheduleResponseFromSchedule() {
+    List<Schedule> scheduleList =
+        List.of(
+            Schedule.builder().type(ScheduleType.OCCASIONALLY).build(),
+            Schedule.builder()
+                .type(ScheduleType.WEEKLY)
+                .priceIncreasedPercentageOnFestive(1)
+                .weekDay(DayOfWeek.of(1))
+                .build(),
+            Schedule.builder()
+                .type(ScheduleType.DAILY)
+                .priceIncreasedPercentageOnFestive(1)
+                .enabled(false)
+                .build(),
+            Schedule.builder().type(ScheduleType.WORKDAY).enabled(false).build(),
+            Schedule.builder().type(ScheduleType.WEEKEND).build());
+    scheduleList.forEach(schedule -> schedule.setId(UUID.randomUUID()));
+    return scheduleList.stream().map(Arguments::of);
+  }
+}
